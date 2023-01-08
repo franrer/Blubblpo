a5356fad6fdf83b79e19281be6d8771fb98178bc
franrer <85014312+franrer@users.noreply.github.com>
2023-01-08 18:38:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+//Diese PHP-Datei Sammelt Dateien um die Seite Home zu erstellen
+include "./php/db.php";
+include "./html/templates/head.html";
+include "./html/main_css.html";
+include "./html/templates/header.html";
+include "./html/home.html";
+include "./html/templates/footer.html";
+include "./html/templates/foot.html";
+
+?>
