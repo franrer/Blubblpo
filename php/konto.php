532da235d869893ec96b71e16a5c8b4ff9050bad
franrer <franzi-hahn@gmx.net>
2023-01-08 18:49:14 +0100

    Blubb

--- php/konto.php
+++ php/konto.php
@@ -5,25 +5,25 @@ if(isset($_GET['btnLogout'])){
   setcookie("email", "", time()-(60*60*24*7),"/");
   setcookie("username", "", time()-(60*60*24*7),"/");
   setcookie("privalage", "", time()-(60*60*24*7),"/");
-  echo "<script>window.location = 'http://localhost/dhbw/tinf20_ludomo_app/php/konto.php'</script>";
+  echo "<script>window.location = 'http://localhost/dhbw/Blubb/php/konto.php'</script>";
 }
 // Wenn der Button Delete geklickt wird, dann wird der User in der Selbe Reihe wie der Button gelöscht
 if(isset($_GET['btnDelete'])){
   $btnNmbr = $_GET['btnDelete'];
-  $conn = mysqli_connect("localhost","root","","ludomo");   //Verbindung zu DB aufbauen
+  $conn = mysqli_connect("localhost","root","","blubb");   //Verbindung zu DB aufbauen
   $sql = "DELETE FROM user_tabelle WHERE ID=$btnNmbr";      //SQL Statement zum löschen einer User
   mysqli_query($conn, $sql);                                //SQL Statement ausführen
   mysqli_close($conn);                                      //SQL Ausführung abschließen
-  echo "<script>window.location = 'http://localhost/dhbw/tinf20_ludomo_app/php/konto.php'</script>";
+  echo "<script>window.location = 'http://localhost/dhbw/Blubb/php/konto.php'</script>";
 }
 // Wenn der Button Upgrade geklickt wird, dann wird der User in der Selbe Reihe wie der Button zum Admin gemacht
 if(isset($_GET['btnUpgrade'])){
   $btnNmbr = $_GET['btnUpgrade'];
-  $conn = mysqli_connect("localhost","root","","ludomo");  //Verbindung zu DB aufbauen
+  $conn = mysqli_connect("localhost","root","","blubb");  //Verbindung zu DB aufbauen
   $sql = "UPDATE user_tabelle SET privalage='10' WHERE ID = $btnNmbr";
   mysqli_query($conn, $sql);
   mysqli_close($conn);
-  echo "<script>window.location = 'http://localhost/dhbw/tinf20_ludomo_app/php/konto.php'</script>";
+  echo "<script>window.location = 'http://localhost/dhbw/Blubb/php/konto.php'</script>";
 }
 // Wenn der Button Downgrade geklickt wird, dann wird der User in der Selbe Reihe wie der Button zdie Admin Rechte weg genommen
 if(isset($_GET['btnDowngrade'])){
